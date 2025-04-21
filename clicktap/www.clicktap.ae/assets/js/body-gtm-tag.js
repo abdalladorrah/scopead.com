d124c8395a75252cb677acee34f97dddeba310b7
abdalladorrah <140260297+abdalladorrah@users.noreply.github.com>
2025-04-21 22:37:21 +0300

    Delete clicktap directory

--- clicktap/www.clicktap.ae/assets/js/body-gtm-tag.js
+++
@@ -1,3 +0,0 @@
-// gtm-noscript.js
-
-document.write('<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WVDD663H" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>');
